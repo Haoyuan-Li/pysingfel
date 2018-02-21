fd270ca0afd079b817aa6a426365a017f1cfb989
Haoyuan-Li-93 <hyli16@stanford.com>
2018-02-21 01:16:31 +0000

    Change the theme of the doc.

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,9 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+# Set the sphinx theme
+import sphinx_rtd_theme
+
 
 # -- General configuration ------------------------------------------------
 
@@ -53,7 +56,7 @@ master_doc = 'index'
 # General information about the project.
 project = u'pysingfel'
 copyright = u'2018, Haoyuan Li, Zhaoyou Wang'
-author = u'Haoyuan Li, Zhaoyou Wang'
+author = u'Haoyuan Li, Zhaoyou Wang, Chun Hong Yoon'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -88,13 +91,16 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+#html_theme_options = {
+#    "rightsidebar": "true",
+#    "relbarbgcolor": "black"
+#}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -148,7 +154,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'pysingfel.tex', u'pysingfel Documentation',
-     u'Haoyuan Li, Zhaoyou Wang', 'manual'),
+     u'Haoyuan Li, Zhaoyou Wang, Chun Hong Yoon', 'manual'),
 ]
 
 
