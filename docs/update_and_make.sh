7ebe482bea4d53a7d21e2dd3c27760a7cddfd8c4
Haoyuan-Li-93 <hyli16@stanford.com>
2018-02-26 18:47:40 +0000

    Remove redundent files and codes

--- docs/update_and_make.sh
+++ docs/update_and_make.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 
-## Save the original index file for safety consideration
-mv ./index.rst ./index.rst-backup
-mv ./theory.rst ./theory.rst-backup
-mv ./modules.rst ./modules.rst-backup
-mv ./example.rst ./example.rst-backup
-mv ./start.rst ./start.rst-backup
-
-
-## replace the original index file with the newer one
-cp ./index.txt ./index.rst
-cp ./theory.txt ./theory.rst
-cp ./modules.txt ./modules.rst
-cp ./example.txt ./example.rst
-cp ./start.txt ./start.rst
-
 ## compile
 sphinx-build -b html -D imgmath_latex="/usr/bin/latex" . _build/html
